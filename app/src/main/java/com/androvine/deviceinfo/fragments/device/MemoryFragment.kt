bb56c69a1a83c36043ba26d1ea57edb29fa0099c
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-02-05 20:16:20 +0600

    Basic structure

---
+++ app/src/main/java/com/androvine/deviceinfo/fragments/device/MemoryFragment.kt
@@ -0,0 +1,21 @@
+package com.androvine.deviceinfo.fragments.device
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.androvine.deviceinfo.R
+
+
+class MemoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_memory, container, false)
+    }
+
+}
