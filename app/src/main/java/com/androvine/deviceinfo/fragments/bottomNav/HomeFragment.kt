2d0c9eb08c2fffd52cffee6018d4798456cb7935
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-02-06 14:21:13 +0600

    Test Data

--- app/src/main/java/com/androvine/deviceinfo/fragments/bottomNav/HomeFragment.kt
+++ app/src/main/java/com/androvine/deviceinfo/fragments/bottomNav/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.androvine.deviceinfo.fragments.bottomNav
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +17,33 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val brand = Build.BRAND
+        val model = Build.MODEL
+        val device = Build.DEVICE
+        val product = Build.PRODUCT
+        val manufacturer = Build.MANUFACTURER
+        val hardware = Build.HARDWARE
+        val board = Build.BOARD
+        val bootloader = Build.BOOTLOADER
+        val display = Build.DISPLAY
+        val fingerprint = Build.FINGERPRINT
+        val host = Build.HOST
+        val id = Build.ID
+        val tags = Build.TAGS
+        val type = Build.TYPE
+        val user = Build.USER
+        val codename = Build.VERSION.CODENAME
+        val incremental = Build.VERSION.INCREMENTAL
+        val release = Build.VERSION.RELEASE
+
+
+
     }
 
 
