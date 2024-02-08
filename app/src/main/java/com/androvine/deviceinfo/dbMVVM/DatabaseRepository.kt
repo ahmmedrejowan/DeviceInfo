932e9d4b7eeda24567267bf777bc5ab44d758a6c
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-02-08 13:48:48 +0600

    Database MVVM

--- app/src/main/java/com/androvine/deviceinfo/dbMVVM/DatabaseRepository.kt
+++ app/src/main/java/com/androvine/deviceinfo/dbMVVM/DatabaseRepository.kt
@@ -1,6 +1,10 @@
 package com.androvine.deviceinfo.dbMVVM
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.androvine.deviceinfo.dataClasses.CpuDataModel
+import com.androvine.deviceinfo.dataClasses.DeviceDataModel
 import com.androvine.deviceinfo.databases.CpuDatabaseHelper
 import com.androvine.deviceinfo.databases.DeviceDatabaseHelper
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +15,13 @@ class DatabaseRepository(private val context: Context) {
     private val cpuDatabaseHelper = CpuDatabaseHelper(context)
     private val deviceDatabaseHelper = DeviceDatabaseHelper(context)
 
+    private val _cpuDataModel = MutableLiveData<CpuDataModel?>()
+    val cpuDataModel: LiveData<CpuDataModel?> get() = _cpuDataModel
+
+    private val _deviceDataModel = MutableLiveData<DeviceDataModel?>()
+    val deviceDataModel: LiveData<DeviceDataModel?> get() = _deviceDataModel
+
+
     suspend fun copyDatabaseFromAssets() {
 
         withContext(Dispatchers.IO) {
@@ -18,6 +29,27 @@ class DatabaseRepository(private val context: Context) {
             deviceDatabaseHelper.copyDatabaseFromAssets(context)
         }
 
+    }
+
+    suspend fun getCpuDataByModel(model: String) {
+        withContext(Dispatchers.IO) {
+            val cpuData = cpuDatabaseHelper.getCpuDataByModel(model)
+            _cpuDataModel.postValue(cpuData)
+        }
+    }
+
+    suspend fun getDeviceDataByModel(model: String) {
+        withContext(Dispatchers.IO) {
+            val deviceData = deviceDatabaseHelper.getDeviceByModel(model)
+            _deviceDataModel.postValue(deviceData)
+        }
+    }
+
+    suspend fun getDeviceDataByDevice(device: String) {
+        withContext(Dispatchers.IO) {
+            val deviceData = deviceDatabaseHelper.getDeviceByDevice(device)
+            _deviceDataModel.postValue(deviceData)
+        }
     }
 
 
--- app/src/main/java/com/androvine/deviceinfo/dbMVVM/DatabaseViewModel.kt
+++ app/src/main/java/com/androvine/deviceinfo/dbMVVM/DatabaseViewModel.kt
@@ -6,11 +6,33 @@ import kotlinx.coroutines.launch
 
 class DatabaseViewModel(private val repository: DatabaseRepository) : ViewModel() {
 
+    val cpuDataModel = repository.cpuDataModel
+    val deviceDataModel = repository.deviceDataModel
 
     fun copyDatabaseFromAssets() {
         viewModelScope.launch {
             repository.copyDatabaseFromAssets()
         }
     }
+
+    fun getCpuDataByModel(model: String) {
+        viewModelScope.launch {
+            repository.getCpuDataByModel(model)
+        }
+    }
+
+    fun getDeviceDataByModel(model: String) {
+        viewModelScope.launch {
+            repository.getDeviceDataByModel(model)
+        }
+    }
+
+    fun getDeviceDataByDevice(device: String) {
+        viewModelScope.launch {
+            repository.getDeviceDataByDevice(device)
+        }
+    }
+
+
 
 }
