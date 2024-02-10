42c98dd9fd6a3cdce1517716c2928a139a3a6248
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-02-11 01:26:01 +0600

    cpu info

--- app/src/main/java/com/androvine/deviceinfo/fragments/device/CpuFragment.kt
+++ app/src/main/java/com/androvine/deviceinfo/fragments/device/CpuFragment.kt
@@ -1,11 +1,14 @@
 package com.androvine.deviceinfo.fragments.device
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.androvine.deviceinfo.R
 import com.androvine.deviceinfo.databinding.FragmentCpuBinding
 import com.androvine.deviceinfo.databinding.FragmentOsBinding
@@ -25,6 +28,7 @@ class CpuFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -81,7 +85,31 @@ class CpuFragment : Fragment() {
         }
 
 
+        binding.parentLayout.setOnTouchListener { _, event ->
+            // Check if the touch event is outside the TextViews
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                // Clear text selection when the user touches outside
+                clearTextSelection(view)
+            }
+            false
+        }
+
+
     }
 
+    private fun clearTextSelection(view: View) {
+        // Iterate through all TextViews and clear text selection
+        clearTextSelectionInView(view)
+    }
+
+    private fun clearTextSelectionInView(view: View) {
+        if (view is TextView) {
+            view.clearFocus()
+        } else if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                clearTextSelectionInView(view.getChildAt(i))
+            }
+        }
+    }
 
 }
